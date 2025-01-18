908a0beca9ef3455a8743c03a25dd78d102671cd
Vranda Garg <gargvranda963@gmail.com>
2025-01-18 15:15:26 +0530

    using CircularArray . optimized solution

---
+++ DSA/14 Deque/02 Array Implementation of Dequeue/usingCircularArray.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// initially front=0, rear=-1
+
+struct Deque
+{
+    int size, cap, front, rear;
+    int *arr;
+    Deque(int c)
+    {
+        size = 0; // no. of elements in deque
+        cap = c;  // capacity of deque
+        front = 0;
+        arr = new int[cap];
+        rear = (front + size - 1) % cap; // rear = -1 initially
+    }
+
+    bool isFull()
+    {
+        return size == cap;
+    }
+
+    bool isEmpty()
+    {
+        return size == 0;
+    }
+
+    void insertFront(int x)
+    {
+        if (isFull())
+        {
+            cout << "OVERFLOW" << endl;
+            return;
+        }
+
+        front = (front + cap - 1) % cap; // to make it circular
+        arr[front] = x;
+        size++;
+    }
+
+    void deleteFront()
+    {
+        if (isEmpty())
+        {
+            cout << "UNDERFLOW" << endl;
+            return;
+        }
+
+        front = (front + 1) % cap; // to make it circular
+        size--;
+    }
+
+    void insertRear(int x)
+    {
+        if (isFull())
+        {
+            cout << "OVERFLOW" << endl;
+            return;
+        }
+
+        rear = (rear + 1) % cap; // to make it circular
+        arr[rear] = x;
+        size++;
+    }
+
+    void deleteRear()
+    {
+        if (isEmpty())
+        {
+            cout << "UNDERFLOW" << endl;
+            return;
+        }
+
+        rear = (rear + cap - 1) % cap; // to make it circular
+        size--;
+    }
+
+    int getRear()
+    {
+        if (isEmpty())
+        {
+            cout << "UNDERFLOW" << endl;
+            return -1;
+        }
+        return arr[rear];
+    }
+
+    int getFront()
+    {
+        if (isEmpty())
+        {
+            cout << "UNDERFLOW" << endl;
+            return -1;
+        }
+        return arr[front];
+    }
+};
+
+int main()
+{
+    Deque dq(5);
+    dq.insertFront(10);
+    dq.insertFront(20);
+    dq.insertRear(30);
+    dq.insertRear(40);
+    cout << dq.getFront() << endl;
+    cout << dq.getRear() << endl;
+    dq.deleteFront();
+    cout << dq.getFront() << endl;
+    dq.deleteRear();
+    cout << dq.getRear() << endl;
+    return 0;
+}
