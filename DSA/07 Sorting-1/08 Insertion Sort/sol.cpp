727a0b31086ec2e1e9b669a4a9d20345288093d9
Vranda Garg <gargvranda963@gmail.com>
2025-01-30 19:05:43 +0530

    Insrtion sort done

--- DSA/07 Sorting-1/08 Insertion Sort/sol.cpp
+++ DSA/07 Sorting-1/08 Insertion Sort/sol.cpp
@@ -13,3 +13,45 @@
 // 6th iteration: [5,10,20,30,40,60]
 //                 - -  -  -   -  -
 // O/P: [5,10,20,30,40,60]
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void InsertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int x = i;     // current
+        int j = i - 1; // current-1
+
+        while (j >= 0 && arr[x] < arr[j])
+        {
+
+            swap(arr[j], arr[x]);
+            x--;
+            j--;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {20, 5, 40, 60, 10, 30};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Sorting: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    InsertionSort(arr, n);
+
+    cout << "\nAfter Sorting: ";
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
