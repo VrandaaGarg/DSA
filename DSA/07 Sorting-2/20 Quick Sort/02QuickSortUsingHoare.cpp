349ea93552c4e3d6769fe3d031b3c92bfb6d6e58
Vranda Garg <gargvranda963@gmail.com>
2025-02-04 12:09:19 +0530

    partition using hoare is done

--- DSA/07 Sorting-2/20 Quick Sort/02QuickSortUsingHoare.cpp
+++ DSA/07 Sorting-2/20 Quick Sort/02QuickSortUsingHoare.cpp
@@ -4,28 +4,28 @@ using namespace std;
 int HoarePartition(int arr[], int l, int h)
 {
     int pivot = arr[l];
-    int i = l - 1, j = h + 1; // is as -1 and j has size of array
-    while (true)              // stop when i and j cross each other
+    int i = l - 1, j = h + 1;
+    while (true)
     {
         do
         {
             i++;
-        } while (arr[i] < pivot); // find the element greater than pivot
+        } while (arr[i] < pivot);
         do
         {
             j--;
-        } while (arr[j] > pivot); // find the element smaller than pivot
+        } while (arr[j] > pivot);
         if (i >= j)
-            return j;         // return the index of the element which is smaller than pivot
-        swap(arr[i], arr[j]); // swap the elements
+            return j; // return the index of the element which is smaller than pivot
+        swap(arr[i], arr[j]);
     }
 }
 
 void qSort(int arr[], int l, int h)
 {
-    if (l < h) // if l becomes equal to h, it means we have only
+    if (l < h)
     {
-        int p = HoarePartition(arr, l, h); // p is the new position of pivot element
+        int p = HoarePartition(arr, l, h); // p is the position of element index which is just smaller than pivot
         qSort(arr, l, p);                  // as p is not at its correct position thats why it is from l to p
         qSort(arr, p + 1, h);              // and this is from p+1 to h
     }
@@ -33,6 +33,9 @@ void qSort(int arr[], int l, int h)
 
 // In quick sort your main function is partition function
 // In merge sort your main function is merge function
+// Hoare partition is much faster than omuto partition
+// Some theory says that Hoare partition is 3 times faster than lomuto partition
+// Its not stable
 
 int main()
 {
